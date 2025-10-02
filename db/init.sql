7e69609936a523da7b574dbbb03ea3c2d9ef8fae
Niels Kjeldbjerg <353468@viauc.dk>
2025-10-02 10:38:30 +0200

    initial commit (setup)

---
+++ db/init.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE IF NOT EXISTS entities
+(
+    id
+    TEXT
+    PRIMARY
+    KEY,
+    data
+    TEXT
+);
+INSERT INTO entities (id, data)
+VALUES ('test1', 'hello from postgres') ON CONFLICT DO NOTHING;
